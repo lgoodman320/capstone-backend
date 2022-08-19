f7e5372942e1b978b848c2a331755e498c1a0572
@iamlarrygoodman <103057796+lgoodman320@users.noreply.github.com>
2022-08-19 09:11:21 -0400

    Update databaseURL and options

--- db/db.js
+++ db/db.js
@@ -1,3 +1,32 @@
-//
+// get Sequelize from npm packages
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres://larrygoodman@localhost:5432", {logging: false});
+
+// initialize options to an empty object
+let options = {};
+
+// set the databaseURL to the process.env.DATABASE_URL
+// to work with heroku's database
+let databaseURL = process.env.DATABASE_URL;
+
+// if running database on localhost
+if (!databaseURL) {
+    // set databaseURL to the localhost database URL
+    databaseURL = "postgres://larrygoodman@localhost:5432/capstone";
+    // set options for database running on localhost
+    options = {
+        // set logging to false
+        logging: false,
+    };
+} else {
+    // set options for database running on heroku
+    options.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    };
+}
+// initialize an instance of Sequelize to connect to the database
+// passing the URI options as arguments
+const db = new Sequelize(databaseURL, options);
+
