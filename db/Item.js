7a877e695b65594510f611632884c9fd9eb89361
@iamlarrygoodman <103057796+lgoodman320@users.noreply.github.com>
2022-08-21 15:07:03 -0400

    Add Store model and notes.txt file

---
+++ db/Item.js
@@ -0,0 +1,21 @@
+// Import Sequelize's builtin data types
+const { DataTypes } = require("sequelize");
+const { db } = require("./db");
+
+// Create Item model
+const Item - (db) => {
+    return db.define("item", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        aisle: DataTypes.STRING,
+        base_price: DataTypes.DECIMAL,
+        categorie_id: DataTypes.INTEGER,
+        categorie_name: DataTypes.STRING,
+        fulfillment_store_number: DataTypes.INTEGER,
+        name: DataTypes.STRING,
+        usa_snap_eligible: DataTypes.BOOLEAN,
+    });
+};
